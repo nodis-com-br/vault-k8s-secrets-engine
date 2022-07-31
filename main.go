01b27b60a474a70f1c88d727cc79440174387f5b
ptonini <pedro.tonini@nodis.com.br>
2022-07-31 03:25:06 -0300

    chore: updated header in main.go

--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 /*
- * Vault Kubernetes Secrets Engine
- * Open source kubernetes credentials manager for Hashicorp Vault
- * Copyright (c) 2022 Pedro Tonini
+ * Vault Kubernetes Secrets Engine, open source software quality management tool.
+ * Copyright (C) 2022 Pedro Tonini
  * mailto:pedro DOT tonini AT hotmail DOT com
  *
- * Vault Kubernetes Secrets Engine is free software;
- * you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation;
- * either version 3 of the License, or (at your option)
- * any later version.
+ * Vault Kubernetes Secrets Engine is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
  *
- * Vault Kubernetes Secrets Engine is distributed in the hope
- * that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General
- * Public License for more details.
+ * Vault Kubernetes Secrets Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
 package main
 
 import (
